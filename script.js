729bb039e094fbdcc7251ffaef0709ecd22a3957
yadhuks42 <yadhuks42@gmail.com>
2025-03-04 13:56:58 +0000

    Implement all operations, Create a basic display.

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-const digit = document.querySelector(".numBtn");
-const equals = document.querySelector(".equals");
+const digit = document.querySelector(`.numBtn`);
+const operator = document.querySelector(`.opBtn`);
+const equals = document.querySelector(`.equals`);
+const visual = document.querySelector(`.display`);
+const entry = document.createElement(`p`);
+visual.appendChild(entry);
 
 let input = [];
-digit.addEventListener("click",(event) =>{
-input.push(event.target.textContent);
+let operand = [];
+
+digit.addEventListener(`click`,(event) =>{
+operand.push(event.target.textContent);
+let display = operand.join(``);
+entry.textContent=display;
+//console.log(display);
 });
 
-equals.addEventListener("click",(event) =>{
-    let nums = input.slice();
-    operate(nums);
-})
+operator.addEventListener(`click`,(event) =>{
+    input.push(operand.join(``),event.target.textContent);
+    display=input.join(``);
+    entry.textContent=display;
+    //console.log(display);
+    operand = [];
+});
 
+equals.addEventListener(`click`,(event) =>{
+    input.push(operand.join(``));
+    display=input.join(``);
+    entry.textContent=display;
+    //console.log(display);
+    operand = [];
+    let nums = input.slice();
+    input=[];
+    entry.textContent=operate(nums);
+    //console.log(`final answer:`, operate(nums));
+
+});
 
 function sum(...theArgs){
     return theArgs.reduce((first,current) =>{
@@ -19,56 +43,48 @@ function sum(...theArgs){
 }
 
 function subtract(a,b){
-    return a-b;
+    return Number(a) - Number(b);
 }
 
 function multiply(...theArgs){
     return theArgs.reduce((first,current) =>{
-        return first * current;
+        return Number(first) * Number(current);
     },1);
 }
 
 function divide(a,b){
-    return a/b;
+    return Number(a)/Number(b);
 }
 
 function operate (arr){
-    if(arr.length>3){
+    if(arr.length >= 3){
         let count = (arr.length-1)/2;
         for(let i = 0;i<count;i++){
-            //console.log(arr, arr.length);
+            console.log(arr, arr.length);
             let chunk = selectOperate(arr.slice(0,3));
-            console.log(chunk);
             arr.splice(0,3,chunk);
-            console.log(arr);
-            //console.log(input);
+           //console.log(input);
         }
+        return arr[0];
     }
-    else if(arr.length===3){
-        console.log(arr, arr.length);
-        arr = selectOperate(arr);
-        console.log(arr);
-    }
-    else if (arr.length<3){
-        console.log("smaller");
+    else{
+        console.log(`smaller`);
     }
 }
 
 function selectOperate(arr){
 
     if(arr.includes(`+`)){
-        //console.log(`answer`, sum(arr[0],arr[2]));
-        return sum(arr[0],arr[2]);
-        
+        return sum(arr[0],arr[2]);     
     }
     if(arr.includes(`-`)){
-        console.log(subtract(arr[0],arr[2]));
+        return subtract(arr[0],arr[2]);
     }
     if(arr.includes(`*`)){
-        console.log(multiply(arr[0],arr[2]));
+        return multiply(arr[0],arr[2]);
     }
     if(arr.includes(`/`)){
-        console.log(divide(arr[0],arr[2]));
+        return divide(arr[0],arr[2]);
     }
         
 }
